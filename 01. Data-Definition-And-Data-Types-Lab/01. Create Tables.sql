bdfe6ce513f5eff45b03c6602b40783d23e8fd76
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-02-16 00:27:44 +0200

    update

---
+++ 01. Data-Definition-And-Data-Types-Lab/01. Create Tables.sql
@@ -0,0 +1,17 @@
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50)
+);
+
+
+CREATE TABLE categories (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE products(
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(50) NOT NULL,
+category_id INT NOT NULL
+);
