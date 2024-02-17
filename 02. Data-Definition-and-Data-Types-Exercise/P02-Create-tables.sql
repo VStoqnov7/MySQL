d91d5022b527517c55fa7a82a6f839c9366173f3
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-02-17 21:34:50 +0200

    update

---
+++ 02. Data-Definition-and-Data-Types-Exercise/P02-Create-tables.sql
@@ -0,0 +1,13 @@
+USE `minions`;
+
+CREATE TABLE `minions` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(255),
+    `age` INT
+);
+
+
+CREATE TABLE `towns`(
+	`town_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL
+);
