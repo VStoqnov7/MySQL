16cfd877419a6d64200d36fc8444c4066f2c4d67
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-02-22 00:29:35 +0200

    update

--- 04. Basic-CRUD-Exercise/P03-Find-salary-of-each-employee.sql
+++ 04. Basic-CRUD-Exercise/P03-Find-salary-of-each-employee.sql
@@ -2,6 +2,6 @@ USE `soft_uni`;
 
 SELECT `first_name`, 
 	`last_name`, 
-        `salary` 
-        FROM `employees`
-        ORDER BY `employee_id`;
+	`salary` 
+	FROM `employees`
+	ORDER BY `employee_id`;
