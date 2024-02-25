8e66ef106d96eff36cf31f685d13d81e6578ef19
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-02-26 00:52:19 +0200

    update

--- 06. Built-in-Functions-Exercise/P02-Find-names-of-all-employees-by-last-name.sql
+++ 06. Built-in-Functions-Exercise/P02-Find-names-of-all-employees-by-last-name.sql
@@ -2,5 +2,5 @@ USE `soft_uni`;
 
 SELECT `first_name`, `last_name`
 	FROM `employees`
-    WHERE LOWER(last_name) LIKE '%ei%'
+	WHERE LOWER(last_name) LIKE '%ei%'
 	ORDER BY `employee_id`;
---
+++ 06. Built-in-Functions-Exercise/P03-Find-first-names-of-all-employess.sql
@@ -0,0 +1,9 @@
+USE `soft_uni`;
+
+SELECT `first_name` from `employees`
+	WHERE `department_id` IN (3,10)
+	AND 
+	year(`hire_date`) BETWEEN 1995 and 2005
+	ORDER BY `employee_id`;
+    
+    
