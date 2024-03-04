f0f7082f9f778ee8e489938dbf337cadc15dbc72
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-03-04 02:51:54 +0200

    update

---
+++ 10. Table-Relations-Exercise/P09-Peaks-in-rila.sql
@@ -0,0 +1,8 @@
+USE `geography`;
+    
+SELECT m.`mountain_range`, p.`peak_name`, p.`elevation` AS `peak_elevation`
+	FROM `mountains` AS m
+	JOIN `peaks` AS p
+	ON p.`mountain_id` = m.`id`
+	WHERE m.`mountain_range` = "Rila"
+	ORDER BY p.`elevation` DESC;
