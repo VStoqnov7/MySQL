73b8bb00c32e87f2ae04fa06b7500345ddbfc2b7
Ventsislav Stoyanov <122936061+VStoqnov7@users.noreply.github.com>
2024-03-06 13:37:29 +0200

    update

--- 06. Built-In-Functions-Exercise/P16-Orders-table.sql
+++
@@ -1,7 +0,0 @@
-USE `orders`;
-
-SELECT `product_name`, 
-	`order_date`,
-	adddate(`order_date`, interval 3 day) as pay_due, 
-	adddate(`order_date`, interval 1 month) as deliver_due
-	FROM `orders`;
