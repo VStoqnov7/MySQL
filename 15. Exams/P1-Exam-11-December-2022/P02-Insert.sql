53ed183dbe47becb2887002164df6038d714c339
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-03-12 04:53:15 +0200

    update

--- 15. Exams/P1-Exam-11-December-2022/P02-Insert.sql
+++ 15. Exams/P1-Exam-11-December-2022/P02-Insert.sql
@@ -4,7 +4,7 @@ INSERT INTO `airplanes`(`model`,
 		`passengers_capacity`,
 		`tank_capacity`,
 		`cost`)
-	SELECT CONCAT(REVERSE(`first_name`),797) AS `model`,
+		SELECT CONCAT(REVERSE(`first_name`),797) AS `model`,
 		LENGTH(`last_name`) * 17 AS `passengers_capacity`,
 		`id` * 790 AS `tank_capacity`,
 		LENGTH(`first_name`) * 50.6 AS `cost`
