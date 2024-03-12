4672f227700afb00f89e2782dfa282efd73e7785
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-03-12 04:52:22 +0200

    update

--- 15. Exams/P1-Exam-11-December-2022/P01-Table-design.sql
+++ 15. Exams/P1-Exam-11-December-2022/P01-Table-design.sql
@@ -4,57 +4,55 @@ USE `airlines_db`;
 
 CREATE TABLE `countries`(
 	`id` INT PRIMARY KEY AUTO_INCREMENT,
-    `name` VARCHAR(30) NOT NULL UNIQUE,
-    `description` TEXT,
-    `currency` VARCHAR(5) NOT NULL
+	`name` VARCHAR(30) NOT NULL UNIQUE,
+	`description` TEXT,
+	`currency` VARCHAR(5) NOT NULL
 );
 
 CREATE TABLE `airplanes`(
 	`id` INT PRIMARY KEY AUTO_INCREMENT,
-    `model` VARCHAR(50) NOT NULL UNIQUE,
-    `passengers_capacity` INT NOT NULL,
-    `tank_capacity` DECIMAL(19,2) NOT NULL,
-    `cost` DECIMAL(19,2) NOT NULL
+	`model` VARCHAR(50) NOT NULL UNIQUE,
+	`passengers_capacity` INT NOT NULL,
+	`tank_capacity` DECIMAL(19,2) NOT NULL,
+	`cost` DECIMAL(19,2) NOT NULL
 );
 
 CREATE TABLE `passengers`(
 	`id` INT PRIMARY KEY AUTO_INCREMENT,
-    `first_name` VARCHAR(30) NOT NULL,
-    `last_name` VARCHAR(30) NOT NULL,
-    `country_id` INT NOT NULL,
-    CONSTRAINT fk_passengers_countries
-    FOREIGN KEY (country_id)
-    REFERENCES countries(id)
+	`first_name` VARCHAR(30) NOT NULL,
+	`last_name` VARCHAR(30) NOT NULL,
+	`country_id` INT NOT NULL,
+	CONSTRAINT fk_passengers_countries
+	FOREIGN KEY (country_id)
+	REFERENCES countries(id)
 );
 
 CREATE TABLE `flights`(
 	`id` INT PRIMARY KEY AUTO_INCREMENT,
-    `flight_code` VARCHAR(30) NOT NULL UNIQUE,
-    `departure_country` INT NOT NULL,
-    `destination_country` INT NOT NULL,
-    `airplane_id` INT NOT NULL,
-    `has_delay` TINYINT(1),
-    `departure` DATETIME,
-    CONSTRAINT fk_flight_countries1
-    FOREIGN KEY (departure_country)
+	`flight_code` VARCHAR(30) NOT NULL UNIQUE,
+	`departure_country` INT NOT NULL,
+	`destination_country` INT NOT NULL,
+	`airplane_id` INT NOT NULL,
+	`has_delay` TINYINT(1),
+	`departure` DATETIME,
+	CONSTRAINT fk_flight_countries1
+	FOREIGN KEY (departure_country)
 	REFERENCES countries(id),
 	CONSTRAINT fk_flight_countries2
-    FOREIGN KEY (destination_country)
+	FOREIGN KEY (destination_country)
 	REFERENCES countries(id),
 	CONSTRAINT fk_flight_airplanes
-    FOREIGN KEY (airplane_id)
+	FOREIGN KEY (airplane_id)
 	REFERENCES airplanes(id)
-    
-    
 );
 
 CREATE TABLE `flights_passengers`(
 	`flight_id` INT, 
-    `passenger_id` INT,
+	`passenger_id` INT,
 	CONSTRAINT fk_flights_passengers_flights
-    FOREIGN KEY (flight_id)
+	FOREIGN KEY (flight_id)
 	REFERENCES flights(id),
 	CONSTRAINT fk_flights_passengers_passengers
-    FOREIGN KEY (passenger_id)
+	FOREIGN KEY (passenger_id)
 	REFERENCES passengers(id)
 );
