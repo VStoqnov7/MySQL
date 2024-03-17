ba7604d8482057c523e669364b15d76028fc3d22
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-03-17 05:00:18 +0200

    update

---
+++ 15. Exams/P03-Exam-10-June-2023/P06-Students-age.sql
@@ -0,0 +1,11 @@
+USE `universities_db`;
+
+SELECT `first_name`,
+	`last_name`,
+	`age`,
+	`phone`,
+	`email`
+	FROM `students`
+	WHERE `age` >= 21
+	ORDER BY `first_name` DESC, `email`, `id` LIMIT 10;
+        
