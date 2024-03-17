b3bde662cabde8d6d93f679bca208121e54553cf
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-03-17 05:01:30 +0200

    update

---
+++ 15. Exams/P03-Exam-10-June-2023/P08-Students-count.sql
@@ -0,0 +1,12 @@
+USE `universities_db`;
+
+SELECT COUNT(c.`id`) AS `students_count`,
+	u.`name` AS `university_name`
+	FROM `universities` AS u
+	JOIN `courses` AS c 
+	ON u.`id` = c.`university_id`
+	JOIN `students_courses` AS sc
+	ON c.`id` = sc.`course_id`
+	GROUP BY u.`name`
+	HAVING students_count >= 8
+	ORDER BY `students_count` DESC, u.`name` DESC;
