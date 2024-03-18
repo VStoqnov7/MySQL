5042de67e27ee87abc5c3dd3ee413590b3e8845f
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-03-18 04:10:44 +0200

    update

---
+++ 15. Exams/P04-Exam-15-October/P02-Insert.sql
@@ -0,0 +1,8 @@
+USE `restaurant_db`;
+
+INSERT INTO `products`(`name`, `type`, `price`)
+	SELECT CONCAT(`last_name`, ' ', 'specialty') AS `name`,
+		'Cocktail' AS `type`,
+		CEIL(`salary` * 0.01) AS `price`
+	FROM `waiters`
+	WHERE `id` > 6;
