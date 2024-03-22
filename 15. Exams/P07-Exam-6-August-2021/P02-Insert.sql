75bcbf81b0a09a705c66773e3e45e7e5df3833dc
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-03-23 01:04:19 +0200

    update

---
+++ 15. Exams/P07-Exam-6-August-2021/P02-Insert.sql
@@ -0,0 +1,10 @@
+USE `games`;
+
+INSERT INTO `games`(`name`, `rating`, `budget`, `team_id`)
+	SELECT LOWER(REVERSE(SUBSTRING(`name`, 2))) AS `name`,
+		`id` AS `rating`,
+		`leader_id` * 1000 AS `budget`,
+		`id` AS `team_id`
+	FROM `teams` 
+	WHERE `id` BETWEEN 1 AND 9;
+        
