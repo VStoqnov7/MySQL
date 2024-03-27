98395773af46e63405d62ef31da5079385c86444
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-03-28 01:45:36 +0200

    update

---
+++ 15. Exams/P9-Exam-18-Oct-2020/P09-Find-all-information-of-stores.sql
@@ -0,0 +1,15 @@
+USE `softuni`;
+
+SELECT REVERSE(s.`name`) AS `reversed_name`,
+	CONCAT(UPPER(t.`name`), '-', a.`name`) AS `full_address`,
+	COUNT(e.`id`) AS `employees_count`
+	FROM `stores` AS s
+	JOIN `employees` AS e
+	ON s.`id` = e.`store_id`
+	JOIN `addresses` AS a
+	ON s.`address_id` = a.`id`
+	JOIN `towns` AS t
+	ON a.`town_id` = t.`id`
+	GROUP BY s.`name`
+	HAVING `employees_count` >= 1 
+	ORDER BY `full_address`;
