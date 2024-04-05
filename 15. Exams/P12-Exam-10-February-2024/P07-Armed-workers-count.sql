adb141a8713aa0affd8addb29f27ebc13a115324
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-04-06 00:53:33 +0300

    update

---
+++ 15. Exams/P12-Exam-10-February-2024/P07-Armed-workers-count.sql
@@ -0,0 +1,10 @@
+USE `preserves_db`;
+
+SELECT p.`name`,
+	COUNT(w.`id`) AS `armed_workers`
+	FROM `preserves` AS p
+	JOIN `workers` AS w
+	ON p.`id` = w.`preserve_id`
+	WHERE `is_armed` = 1
+	GROUP BY p.`name`
+	ORDER BY `armed_workers` DESC, p.`name`;
